644a6ac2f1418ad5c282569b79a9cae742ddbdc4
LeeTrent <lee.trent.1@gmail.com>
2023-06-20 17:25:08 -0500

    First and last commit

---
+++ GenerateApiKey/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate the API key
+            string apiKey = GenerateApiKey();
+
+            Console.WriteLine("Generated API key: " + apiKey);
+            Console.ReadLine();
+        }
+
+        static string GenerateApiKey()
+        {
+            byte[] apiKeyBytes = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(apiKeyBytes);
+            }
+            return Convert.ToBase64String(apiKeyBytes);
+        }
+    }
+}
